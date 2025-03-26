c82d3f45096978fa5590571834eca4819429090b
Yifei <Wanyh2937@gmail.com>
2025-03-26 17:12:23 -0400

    v0.0.3 Implemented Tennis and Volleyball Phased out old sport management system

--- main.py
+++ main.py
@@ -57,56 +57,88 @@ sports = [
      "academy_athletics_bonus": 15,
      "academy_messages": "You joined the Cricket Academy Program"},
 
+    # HOCKEY
+    {"name": "Hockey",
+     "level": 0,
+     "injury": 0.15,
+     "injury_messages": ["You broke your left arm whilst defending!", "You broke your right arm whilst offending!", "You broke you leg after slipping on the ice!"],
+     "athletics_bonus": 10,
+     "start_messages": ["You have joined your town cricket team!"],
+
+     "academy_name": "Cricket Academy Program",
+     "academy_athletics_bonus": 30,
+     "academy_messages": ["You joined your town hockey team!"]},
+
+    # TENNIS
+    {"name": "Tennis",
+     "level": 0,
+     "injury": 0.05,
+     "injury_messages": ["You broke your left arm after tripping on a ball!", "You broke your right arm whilst serving!", "You broke you leg after slipping on the court!"],
+     "athletics_bonus": 10,
+     "start_messages": ["You started playing tennis at your local community center!"],
+
+     "academy_name": "Cricket Academy Program",
+     "academy_athletics_bonus": 15,
+     "academy_messages": "You joined a Tennis Academy Program!"},
+
+    # VOLLEYBALL
+    {"name": "Volleyball",
+     "level": 0,
+     "injury": 0.10,
+     "injury_messages": ["You broke your left arm whilst serving!", "You broke your right arm whilst hitting the ball!", "You broke you leg after slipping on the court!"],
+     "athletics_bonus": 10,
+     "start_messages": ["You joined your town volleyball team!"],
+
+     "academy_name": "Cricket Academy Program",
+     "academy_athletics_bonus": 15,
+     "academy_messages": "You joined the Volleyball Academy Program!"},
 ]
-print(" Step 3: Initialize Data")
+def FATAL_ERROR(reason):
+    print("=" * terminal_width)
+    print("Uh oh!")
+    print("Something went wrong. Try restarting the app and try again.")
+    print("Provided reason: " + reason)
 
-soccer_academy_chance = (athletics-40)/20
-
-# CRICKET
-cricket_academy_chance = (athletics-30)/20
-
-# HOCKEY
-hockey_injury= 0.10
-hockey_injury_messages = ["You broke your left arm whilst defending!", "You broke your right arm whilst offending!", "You broke you leg after slipping on the ice!"]
-hockey_athletics_bonus = 10
-hockey_start_messages = "You joined your town hockey team!"
-
-hockey_academy_chance = (athletics-50)/20
-hockey_academy_athletics_bonus = 30
-hockey_academy_messages = "You joined the Hockey Academy Program!"
-
-tennis_injury= 0.05
-tennis_injury_messages = ["You broke your left arm after tripping on a ball!", "You broke your right arm whilst serving!", "You broke you leg after slipping on the court!"]
-tennis_athletics_bonus = 10
-tennis_start_messages = "You started playing tennis at your local community center!"
-
-tennis_academy_chance = (athletics-50)/20
-tennis_academy_athletics_bonus = 30
-tennis_academy_messages = "You joined a Tennis Academy Program!"
-
-# VOLLEYBALL
-volleyball_injury= 0.07
-volleyball_injury_messages = ["You broke your left arm whilst serving!", "You broke your right arm whilst hitting the ball!", "You broke you leg after slipping on the court!"]
-volleyball_athletics_bonus = 7
-volleyball_start_messages = "You joined your town volleyball team!"
-
-volleyball_academy_chance = (athletics-30)/20
-volleyball_academy_athletics_bonus = 30
-volleyball_academy_messages = "You joined the Volleyball Academy Program!"
-# SPORT CHANCES
-
-tennis_injury = 0.05
-volleyball_injury = 0.07
-tabletennis_injury = 0.05
-
-
-print("Step 3: PyLife Functions")
-
+def DUMP():
+    print(name)
+    print(age)
+    print(gender)
+    for sport in sports:
+        print(str(sport["level"]))
+# SPORT ACADEMY APPLICATIONS
+def sport_academy(sport):
+    global sports
+    match sport:
+        case "Soccer":
+            if random.random() <= (athletics-30)/20:
+                print("SPORTS: " + sport[0]["academy_messages"])
+            else:
+                print("SPORTS: You did not make the Soccer Academy Program")
+        case "Cricket":
+            if random.random() <= (athletics-50)/20:
+                print("SPORTS: " + sport[1]["academy_messages"])
+            else:
+                print("SPORTS: You did not make the Soccer Academy Program")
+        case "Hockey":
+            if random.random() <= (athletics-50)/20:
+                print("SPORTS: " + sport[2]["academy_messages"])
+            else:
+                print("SPORTS: You did not make the Hockey Academy Program")
+        case "Tennis":
+            if random.random() <= (athletics-50)/20:
+                print("SPORTS: " + sport[3]["academy_messages"])
+            else:
+                print("SPORTS: You did not make the Tennis Academy Program")
+        case "Volleyball":
+            if random.random() <= (athletics-30)/20:
+                print("SPORTS: " + sport[4]["academy_messages"])
+            else:
+                print("SPORTS: You did not make the Volleyball Academy Program")
+        case _:
+            FATAL_ERROR("1001: Invalid field provided by Sport Academy Program")
 def clear():
     clears = lambda: os.system('cls')
     clears()
-
-
 def cycle():
     clear()
     global age
@@ -123,37 +155,36 @@ def cycle():
         print(name + " is born!")
     # SPORT CYCLE
     for sport in sports:
-        if sport['level'] >= 1:
+        if random.random() <= sport['injury']:
+            print("INJURY: " + random.choice(sport["injury_messages"]))
+            print("INJURY: You were not able to play for a season.")
+            # Injury = no bonus points for any sport
+            break
+        if sport['level'] == 1:
             athletics += (sport['athletics_bonus'] + random.randint(-5,2))
-            if random.random() <= sport['injury']:
-                print("INJURY: " + random.choice(sport["injury_messages"]))
-                print("INJURY: You were not able to play for a season.")
+
     play()
 def play():
+    DUMP()
     print("=" * terminal_width)
     print("Stats:")
     print("Athletics: " + str(athletics) + "/" + "200")
     print("-" * terminal_width)
     print("Select an option:")
     print("0: Age 1 Year")
+    print("1. Sports Options")
     
     play_selection = input(">>> ")
-    if play_selection == "0":
-        cycle()
-
-
+    match play_selection:
+        case "0":
+            cycle()
+        case "1":
+            sports_menu()
+        case _:
+            print("Invalid selection. Please try again.")
 def sports_menu():
-
-    # PLAN
-
-    # Sports
-    # 1. Start new sport
-    # 2. Manage current sports
-
     global age
-    global plays_soccer
-    global plays_cricket
-    global plays_hockey
+    global sports
     print("SPORTS")
     print("1. Start a new sport")
     print("2. Manage current sports")
@@ -167,7 +198,7 @@ def sports_menu():
             if age >= 8:
                 # Age 8 Sports
                 print("2. Cricket")
-
+                print("3. Hockey")
             print("Select a sport")
             sport_selection = int(input(">>> "))
 
@@ -175,9 +206,11 @@ def sports_menu():
             if sports[sport_selection-1]["level"] == 0:
                 print("SPORT: " + random.choice(sports[sport_selection]["start_messages"]))
                 sports[sport_selection-1]["level"] = 1
+            play()
 
         else:
             print("You must be at least 6 years old to play a sport!")
+            play()
     elif sport_selection == "2":
         print("MANAGE SPORTS")
         tempbin = []
@@ -192,6 +225,7 @@ def sports_menu():
         print(sportskey[selectedsport] + " Options")
         print("1. Quit")
 
+        # If currently basic for sport
         if sports[selectedsport] == 1:
             # Join academy program
             print("2. Join Advanced Program")
@@ -201,8 +235,10 @@ def sports_menu():
         match sport_selection:
             case "1":
                 sports[selectedsport] = 0
+                DUMP()
+                time.sleep(5)
+                print("SPORT: You have quit " + sports[selectedsport]["name"])
             case "2":
-
                 sports[selectedsport] = 2
                 print("You have joined the academy program for this sport")
             case _:
